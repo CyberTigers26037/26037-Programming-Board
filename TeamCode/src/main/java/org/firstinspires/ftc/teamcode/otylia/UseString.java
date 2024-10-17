8e403bc72a9356fb6d307716d2dbdc20237cdf79
Lia <139262819+92doki@users.noreply.github.com>
2024-10-17 15:37:54 -0700

    Ran through programming board exercises from Learn Java for FTC- Otylia

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/otylia/UseString.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.otylia;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class UseString extends OpMode {
+    @Override
+    public void init() {
+        String myName = "Otylia";
+
+        telemetry.addData("Hello", myName);
+
+        int grade = 96;
+
+        telemetry.addData("Grade", grade);
+    }
+    @Override
+    public void loop() {
+        int x = 5;
+        // x is visible here
+        {
+            int y = 4;
+            // x and y are visible here
+        }
+        // only x is visible here
+    }
+}
