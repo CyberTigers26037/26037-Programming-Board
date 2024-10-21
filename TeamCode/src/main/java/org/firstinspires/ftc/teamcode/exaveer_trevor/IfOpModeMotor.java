3a6beaffdd649875c893d75696e5c2a5fa62cee1
anpark1234 <anthony@anpark.com>
2024-10-21 16:33:53 -0700

    FTC example code :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exaveer_trevor/IfOpModeMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exaveer_trevor/IfOpModeMotor.java
@@ -7,21 +7,34 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-@TeleOp()
-public class IfOpModeMotor {
+
+@TeleOp(name = "Trevor & Exaveer IfOpModeMotor")
+public class IfOpModeMotor extends OpMode {
     private DigitalChannel touchSensor;
     private DcMotor motor;
+    private double ticksPerRotation;
 
-    public void init(HardwareMap hwMap) {
-        touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
+    public void init() {
+        touchSensor = hardwareMap.get(DigitalChannel.class, "touch_sensor");
         touchSensor.setMode(DigitalChannel.Mode.INPUT);
-        motor = hwMap.get(DcMotor.class, "motor");
+        motor = hardwareMap.get(DcMotor.class, "motor");
         motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ticksPerRotation = motor.getMotorType().getTicksPerRev();
     }
+
+    @Override
+    public void loop() {
+
+    }
+
     public boolean isTouchSensorPressed() {
         return !touchSensor.getState();
     }
 
     public void SetMotorSpeed(double speed) {
+        motor.setPower(speed);
+    }
+    public double getMotorRotations() {
+        return motor.getCurrentPosition() / ticksPerRotation;
     }
 }
