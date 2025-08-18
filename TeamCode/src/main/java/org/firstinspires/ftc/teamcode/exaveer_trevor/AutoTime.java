d5a6ce787d9cbb683fa9d0432265eae3528134fe
Exaveer <exjaywalkie@gmail.com>
2025-08-18 16:22:17 -0700

    Java practice from FTC book Exaveer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exaveer_trevor/AutoTime.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exaveer_trevor/AutoTime.java
@@ -30,6 +30,24 @@ public class AutoTime extends OpMode {
 
     @Override
     public void loop() {
-
+        telemetry.addData("State", state);
+        telemetry.addData("Runtime", getRuntime());
+        telemetry.addData("Time in State", getRuntime() - lastTime);
+        switch(state) {
+            case START:
+                if (getRuntime() >= 3.0) {
+                    state = State.SECOND_STEP;
+                    lastTime = getRuntime();
+                }
+                break;
+            case SECOND_STEP:
+                if (getRuntime() >= lastTime + 3.0) {
+                    state = State.DONE;
+                    lastTime = getRuntime();
+                }
+                break;
+            default:
+                telemetry.addData("Auto", "Finished");
+        }
     }
 }
