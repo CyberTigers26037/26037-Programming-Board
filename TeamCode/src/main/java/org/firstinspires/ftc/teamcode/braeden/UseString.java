1d07948b1f30ba79eb1cbb18e3237d62f8879d23
anpark1234 <anthony@anpark.com>
2025-08-17 18:27:33 -0700

    Braeden and Dax exercises

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/braeden/UseString.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.braeden;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Braeden's UseString")
+public class UseString extends OpMode {
+    @Override
+    public void init() {
+        String myName = "Braeden O'Toole";
+        int myGrade = 9;
+        telemetry.addData("Hello", myName);
+        telemetry.addData("Grade", myGrade);
+
+    }
+
+    @Override
+    public void loop() {
+        int x = 5;
+        // x is visible here
+        {
+            int y = 4;
+            // x and y are visible here
+        }
+        // only x is visible here
+    }
+}
