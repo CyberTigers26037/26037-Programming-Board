2e06326df140307cd768d77f50ee25aae932e068
Exaveer <exjaywalkie@gmail.com>
2025-08-25 16:14:54 -0700

    Java practice from FTC book Exaveer

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exaveer/SuperClass.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.exaveer;
+
+public class SuperClass {
+    public String a() {
+        return "a";
+    }
+
+    public String b() {
+        return "b";
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exaveer/TestMotor.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.exaveer;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class TestMotor extends TestItem {
+    private double speed;
+    private DcMotor motor;
+
+    protected TestMotor(String description, double speed, DcMotor motor) {
+        super(description);
+        this.speed = speed;
+        this.motor = motor;
+    }
+
+    @Override
+    public void run(boolean on, Telemetry telemetry) {
+        if (on) {
+            motor.setPower(speed);
+        } else {
+            motor.setPower(0.0);
+        }
+        telemetry.addData("Encoder: ", motor.getCurrentPosition());
+    }
+}
