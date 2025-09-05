47f2a640e9fa07a16b99d2e8f1309c2996204ffa
anthony <greenteacheese123@gmail.com>
2025-09-05 16:26:07 -0700

    adding jacobs code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jacob/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jacob/HelloWorld.java
@@ -1,13 +1,14 @@
 package org.firstinspires.ftc.teamcode.jacob;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp (name="Jacob HelloWorld")
+@Autonomous(name="Jacob HelloWorld")
 public class HelloWorld extends OpMode {
     @Override
     public void init() {
-        telemetry.addData("Hello","World");
+        telemetry.addData("Hello","Jacob");
     }
 
     @Override
