db478d89889588d5c7a6f7a76c85352beaad16a0
anpark1234 <anthony@anpark.com>
2025-09-21 13:05:14 -0700

    Updated Limelight AprilTag test to read AprilTagId.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exaveer/AprilTagLimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exaveer/AprilTagLimelightTest.java
@@ -3,26 +3,18 @@ package org.firstinspires.ftc.teamcode.exaveer;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.IMU;
 
-import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
-import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-
+@SuppressWarnings("unused")
 @TeleOp(name="Exaveer Limelight Test")
 public class AprilTagLimelightTest extends OpMode {
     private Limelight3A limelight;
-    private IMU imu;
 
     @Override
     public void init() {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.pipelineSwitch(0);
-        imu = hardwareMap.get(IMU.class, "imu");
-        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
-        imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
     }
 
     @Override
@@ -32,17 +24,14 @@ public class AprilTagLimelightTest extends OpMode {
 
     @Override
     public void loop() {
-        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-        limelight.updateRobotOrientation(orientation.getYaw());
         LLResult llResult = limelight.getLatestResult();
         if (llResult != null && llResult.isValid()) {
-            Pose3D botPose = llResult.getBotpose();
-            telemetry.addData("Tx", llResult.getTx());
-            telemetry.addData("Ty", llResult.getTy());
-            telemetry.addData("Ta", llResult.getTa());
-            for (LLResultTypes.DetectorResult d : llResult.getDetectorResults()) {
-                telemetry.addData("Tag ID", d.getClassId());
+            for (LLResultTypes.FiducialResult fr : llResult.getFiducialResults()) {
+                telemetry.addData("AprilTag ID", fr.getFiducialId());
             }
+        }
+        else {
+            telemetry.addData("AprilTag ID", "None");
         }
     }
 }
