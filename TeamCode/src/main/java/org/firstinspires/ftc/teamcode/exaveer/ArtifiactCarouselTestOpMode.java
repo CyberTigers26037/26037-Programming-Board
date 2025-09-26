72ef9f33ce16ef3965c0834a7240017084dd252d
Exaveer <exjaywalkie@gmail.com>
2025-09-26 16:20:56 -0700

    Finished Excersise 1 and began excerise 2 - Exaveer W.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exaveer/ArtifiactCarouselTestOpMode.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.exaveer;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@SuppressWarnings("unused")
+@TeleOp(name="Exaveer ArtifiactCarouselTestOpMode")
+public class ArtifiactCarouselTestOpMode extends OpMode {
+    private ArtifactCarousel carousel;
+
+    boolean intakeActive = false;
+
+    @Override
+    public void init() {
+        carousel = new ArtifactCarousel(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.x) {
+            carousel.moveCarouselToIntakePosition(1); //pos1 -90 degrees
+        }
+        if(gamepad1.y) {
+            carousel.moveCarouselToIntakePosition(2); //pos2 -60 degrees
+        }
+        if(gamepad1.b) {
+            carousel.moveCarouselToIntakePosition(3); //pos3 -30 degrees
+        }
+        if(gamepad1.aWasPressed()) {
+            intakeActive = !intakeActive;
+        }
+        telemetry.addData("Active", intakeActive);
+    }
+}
