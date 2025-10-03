c18ddd17be05c1c8b01174da1ada4770206638f7
Exaveer <exjaywalkie@gmail.com>
2025-10-03 14:41:48 -0700

    began working on mecanumDrive - Exaveer W.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exaveer/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exaveer/MecanumDrive.java
@@ -1,15 +1,29 @@
 package org.firstinspires.ftc.teamcode.exaveer;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class MecanumDrive {
-
+    private DcMotor frontLeftDrive = null;
+    private DcMotor backLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+    private DcMotor backRightDrive = null;
 
     public void init(HardwareMap hardwareMap) {
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "backLeftDrive");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
 
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
     }
 
     public void drive(double axial, double lateral, double yaw) {
-
+        // calculate motor power
+        // normalize the values so no wheel power exceeds 100%
+        // send calculated power to wheels
     }
 }
