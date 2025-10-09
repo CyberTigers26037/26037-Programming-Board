e031ad0ac13d3aa53b26e06e9487a7498468f4d0
Nathan <nathann.lee10@gmail.com>
2025-10-09 12:17:19 -0700

    Adding Carousel Test Nettles

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nettles/ArtifactCarouselTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nettles/ArtifactCarouselTestOpMode.java
@@ -8,42 +8,43 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class ArtifactCarouselTestOpMode extends OpMode {
     private ArtifactCarousel carousel;
     boolean intakeActive = false;
+
     @Override
     public void init() {
 //creates a new hardware in the hardware map called carousel
-carousel = new ArtifactCarousel(hardwareMap);
+        carousel = new ArtifactCarousel(hardwareMap);
     }
+
     @Override
-    public void loop(){
-        if (gamepad1.aWasPressed()){
+    public void loop() {
+        if (gamepad1.aWasPressed()) {
             intakeActive = !intakeActive;
-            telemetry.addData("intakeActive",intakeActive );
-        if (intakeActive) {
+            telemetry.addData("intakeActive", intakeActive);
+            if (intakeActive) {
+                if (gamepad1.x) {
+                    carousel.moveCarouselToIntakePosition(1);
+                }
+                if (gamepad1.y) {
+                    carousel.moveCarouselToIntakePosition(2);
+                }
+                if (gamepad1.b) {
+                    carousel.moveCarouselToIntakePosition(3);
+                }
+
+            }
+        }
+        else {
             if (gamepad1.x) {
-                carousel.moveCarouselToIntakePosition(1);
+                carousel.moveCarouselToFirePosition(1);
             }
             if (gamepad1.y) {
-                carousel.moveCarouselToIntakePosition(2);
+                carousel.moveCarouselToFirePosition(2);
             }
             if (gamepad1.b) {
-                carousel.moveCarouselToIntakePosition(3);
-
+                carousel.moveCarouselToFirePosition(3);
             }
 
-        }
-        }else {
-        }
 
-        if(gamepad1.x) {
-            carousel.moveCarouselToFirePosition(1);
         }
-        if (gamepad1.y){
-            carousel.moveCarouselToFirePosition(2);
-        }
-        if(gamepad1.b){
-            carousel.moveCarouselToFirePosition(3);
-        }
-
-
     }
 }
