18dae2621405ed4477a63d4d4062e6a80825cf48
anthony <anthony@anpark.com>
2026-07-06 14:49:21 -0700

    practice java 1-2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tyler/PrimitiveTypes.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.tyler;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class PrimitiveTypes extends OpMode {
+    @Override
+    public void init() {
+        int teamNumber = 26037;
+        double motorSpeed = 0.7;
+        boolean touchSensorPressed = false;
+
+        telemetry.addData("Team Number", teamNumber);
+        telemetry.addData("Motor Speed", motorSpeed);
+        telemetry.addData("Touch Sensor", touchSensorPressed);
+    }
+@Override
+}
