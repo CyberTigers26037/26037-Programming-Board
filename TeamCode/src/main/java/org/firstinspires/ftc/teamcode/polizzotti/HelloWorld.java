5d63a60d0771e16d4e4d91984f33695073b4752d
mpolizzotti <matthew.polizzotti@gmail.com>
2026-08-20 19:08:29 -0700

    feat(polizzotti): add ifOpMode2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/polizzotti/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/polizzotti/HelloWorld.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.ashlee;
+package org.firstinspires.ftc.teamcode.polizzotti;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/polizzotti/IfOpMode2.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.polizzotti;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+public class IfOpMode2 extends OpMode {
+    @Override
+    public void init() {
+    }
+
+    public void loop() {
+        // When the "A" button is pressed.
+        if (gamepad1.a) {
+            telemetry.addData("A Button", "pressed");
+        }
+    }
+}
