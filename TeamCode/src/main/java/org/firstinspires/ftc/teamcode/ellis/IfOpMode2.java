2988ed2460d756295d34ac43d21381e53b4349a4
Nathan <nathann.lee10@gmail.com>
2026-08-24 16:21:45 -0700

    Ellis Hernandez new stuff i tested 8/24/26

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ellis/IfOpMode2.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.ellis;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="ellis ifOpmode2")
+public class IfOpMode2 extends OpMode {
+    @Override
+    public void init() {}
+}
