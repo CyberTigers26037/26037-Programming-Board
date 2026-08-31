672877fbb9b9be1ffdfe4035f00f8e148423429f
26037 Student 1 <github1@millenniumrobotics.org>
2026-08-31 08:45:01 -0700

    New stuff Ellis Hernandez tested 8/28/26

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ellis/IfElseOpMode2.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.ellis;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "ellis IfElseOpMode2")
+public class IfElseOpMode2 extends OpMode {
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.left_stick_y < -0.5) {
+            telemetry.addData("Left stick", "is negative and large");
+        }
+   else if (gamepad1.left_stick_y < 0){
+       telemetry.addData("left stick", "is negitive and small");
+        }
+   else if (gamepad1.left_stick_y < 0.5){
+       telemetry.addData("left stick", "is positive and small");
+   }
+ telemetry.addData("left stick y", gamepad1.left_stick_y);
+   }
+}
