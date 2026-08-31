fc067b867e7197916e9e03f50059c6884ab3cab9
mpolizzotti <matthew.polizzotti@gmail.com>
2026-08-30 19:26:35 -0700

    feat(polizzotti): use TeleOp annotation on exercises

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/polizzotti/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/polizzotti/HelloWorld.java
@@ -1,17 +1,22 @@
 package org.firstinspires.ftc.teamcode.polizzotti;
-
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 @Autonomous(name = "Polizzotti HelloWorld")
 public class HelloWorld extends OpMode {
+    /**
+     * This is called when the driver presses INIT
+     */
     @Override
     public void init() {
-        telemetry.addData("Hello", "World");
+        telemetry.addData("Hello", "Matthew");
     }
 
+    /**
+     * This is called repeatedly while OpMode is playing
+     */
     @Override
     public void loop() {
-
+        // intentionally left blank
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/polizzotti/IfElseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/polizzotti/IfElseOpMode.java
@@ -3,17 +3,17 @@ package org.firstinspires.ftc.teamcode.polizzotti;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name="Polizzotti IfOpMode")
-public class IfOpMode extends OpMode {
+@TeleOp(name="Polizzotti: IfElseOpMode")
+public class IfElseOpMode extends OpMode {
     @Override
-    public void init() {
-    }
+    public void init() {}
 
+    @Override
     public void loop() {
-        // The `y` or `up and down` joystick.
-        // Up is negative && Down is positive
         if (gamepad1.left_stick_y < 0) {
             telemetry.addData("Left stick", " is negative");
+        } else {
+            telemetry.addData("Left stick", " is positive");
         }
 
         telemetry.addData("Left stick y", gamepad1.left_stick_y);
