3242a8af2863b52cfa130a9626f8e8794b110b5f
26037 Student 1 <github1@millenniumrobotics.org>
2026-09-04 08:47:55 -0700

    Committing Richard's code from 8/31

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Miladandrichard/Richard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Miladandrichard/Richard.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.Miladandrichard;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @TeleOp(name="Richard Richard")
 public class Richard extends OpMode {
     @Override
