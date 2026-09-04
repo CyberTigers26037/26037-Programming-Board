67e4453cafa384b1ccea8bbee54fe164f5a5273b
26037 Student 1 <github1@millenniumrobotics.org>
2026-09-04 08:47:04 -0700

    Adding Parker's code from 8/31

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Parker/IfElseOpMode.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.Parker;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@SuppressWarnings("unused")
+@TeleOp(name = "Parker IfElseOpMode")
+public class IfElseOpMode extends OpMode {
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.left_stick_y < 0) {
+            telemetry.addData("Left Stick", "is negative");
+        }
+        else{
+            telemetry.addData("Left Stick", "is positive");
+        }
+
+        telemetry.addData("Left Stick y", gamepad1.left_stick_y);
+    }
+}
