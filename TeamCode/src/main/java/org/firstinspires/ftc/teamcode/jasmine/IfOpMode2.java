458e4a4239f7bac3f333299d92636209240220a0
26037 Student 1 <github1@millenniumrobotics.org>
2026-09-08 15:42:53 -0700

    What Jasmine did on 9/8 today

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jasmine/IfOpMode2.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.jasmine;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Jasmine ifOpMode2")
+public class IfOpMode2 extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.a){
+            telemetry.addData("A Button", "pressed");
+        }
+    }
+}
