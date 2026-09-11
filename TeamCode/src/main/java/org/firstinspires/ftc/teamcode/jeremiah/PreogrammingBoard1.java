847de8384e36d2d937dd9668f2e655ed689eb62a
26037 Student 1 <github1@millenniumrobotics.org>
2026-09-11 16:06:55 -0700

    practiced pragraming

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jeremiah/PreogrammingBoard1.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.jeremiah;
+
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class PreogrammingBoard1 {
+    private DigitalChannel touchSensor;
+
+    public void init(HardwareMap hwMap) {
+        touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
+        touchSensor.setMode(DigitalChannel.Mode.INPUT);
+
+        public boolean getTouchSensorState(){
+            return touchSensor.getState();
+        }
+
+    }
+}
