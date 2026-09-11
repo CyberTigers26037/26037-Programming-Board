1fc74597b2f2ca0c3a056fb80c697c70f2bf9a5b
26037 Student 1 <github1@millenniumrobotics.org>
2026-09-11 16:16:50 -0700

    Code from 9/11/26 Lilliana

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lilliana/RobotLocation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lilliana/RobotLocation.java
@@ -1,4 +1,35 @@
 package org.firstinspires.ftc.teamcode.Lilliana;
 
+import androidx.annotation.NonNull;
+
 public class RobotLocation {
+    double angle;
+
+    public RobotLocation(double angle){
+        this.angle = angle;
+    }
+
+    public double getHeading(){
+        double angle = this.angle;
+        while(angle > 180){
+            angle -= 360;
+        }
+        while(angle < -180){
+            angle +=360;
+        }
+        return angle;
+    }
+
+    @NonNull
+    @Override
+    public String toString(){
+        return "RobotLocation: angle (" + angle +")";
+    }
+
+    public void turn(double angleChange){
+        angle += angleChange;
+    }
+    public void setAngle(double angle){
+        this.angle = angle;
+    }
 }
