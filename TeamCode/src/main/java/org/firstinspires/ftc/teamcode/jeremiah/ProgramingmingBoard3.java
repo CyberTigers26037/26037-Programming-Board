b65f80b426e2fbe6ac8cd5e57a97b8533ed128a9
26037 Student 1 <github1@millenniumrobotics.org>
2026-09-18 15:33:49 -0700

    practice and fixed error.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jeremiah/ProgramingmingBoard3.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.jeremiah;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class ProgramingmingBoard3 {
+    private DigitalChannel touchSensor;
+    private DcMotor motor;
+
+    public void init (HardwareMap hwMap) {
+        touchSensor = hwMap.get(DigitalChannel.class, "touch_sesnsor");
+        touchSensor.setMode(DigitalChannel.Mode.INPUT);
+        motor = hwMap.get(DcMotor.class, "motor");
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public boolean isTouchSensorPressed() {
+        return !touchSensor.getState();
+    }
+
+    public void setMotorSpeed(double speed){
+        motor.setPower(speed);
+    }
+}
