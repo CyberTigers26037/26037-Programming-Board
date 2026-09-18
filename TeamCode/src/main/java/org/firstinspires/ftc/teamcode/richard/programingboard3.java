9391bd59787cfa9e8b0b3dacd7a856a90176098b
26037 Student 1 <github1@millenniumrobotics.org>
2026-09-18 16:23:37 -0700

    capter 7

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/richard/programingboard3.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.richard;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+public class programingboard3 {
+    private DigitalChannel touchSensor;
+    private DcMotor moter;
+
+    public void setTouchSensor(DigitalChannel touchSensor) {
+        this.touchSensor = touchSensor;
+        return;
+
+    }
+
