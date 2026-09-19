6adf3a66d450f8f0535b05a649b57097aa62200a
26037 Student 1 <github1@millenniumrobotics.org>
2026-09-18 19:41:45 -0700

    Milad  read and worked on the code for chapter 7

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MiladM/ProgrammingBoard3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MiladM/ProgrammingBoard3.java
@@ -5,17 +5,12 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class ProgrammingBoard3 {
-    private DigitalChannel touchsensor;
+    private DigitalChannel touchSensor;
     private DcMotor motor;
 
-    public void init(HardwareMap hwMap)  {}
-
-    public DigitalChannel getTouchsensor() {
-        touchsensor = hwMap.get(DigitalChannel.class, "touch_sensor");
+    public void init(HardwareMap hwMap) {
+        touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
         touchSensor.setMode(DigitalChannel.Mode.INPUT);
     }
-
-
-
 }
 
